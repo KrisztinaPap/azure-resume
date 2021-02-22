5168caed6b10d11bdaf5703112a87f40e756e58a
Krisztina Pap <krisztina.pap@outlook.com>
2021-02-22 15:44:21 -0700

    Temporarily removed visitor counter code

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 // Get current year for footer
 
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-
-// Count site visits
-
-/* function visitorCounter() {
-    var counterSpan = document.getElementById("counterSpan");
-    var currentCounterValue = Number(counterSpan.textContent);
-    var updatedCountValue = currentCounterValue;
-    updatedCountValue++;
-    counterSpan.innerHTML = Number(updatedCountValue);
-}
-
-window.onload = visitorCounter; */

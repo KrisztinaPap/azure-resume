b4407216a464beff1505caa20ecfb86ccac9a565
Krisztina Pap <krisztina.pap@outlook.com>
2021-03-05 14:23:43 -0700

    Added navbar and skeleton of projects and contact pages

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Get current year for footer
 
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+
+// Navigation Menu Overlay Functionality
+// Inspiration: https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp
+
+var navigation = document.getElementById("myNav");
+
+/* Open */
+function openNav() {
+    navigation.style.height = "100vh";
+}
+
+/* Close */
+function closeNav() {
+    navigation.style.height = "0vh";
+}
